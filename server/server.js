b6219bb4408a3db4996a93205fcd04602ab4ef52
xyzhrey <xyzhrey@gmail.com>
2026-05-27 23:16:39 +0530

    deployment ready on www

--- server/server.js
+++ server/server.js
@@ -28,10 +28,17 @@ io.on("connection", (socket) => {
     )
 })
 
-server.listen(3000, "0.0.0.0" , () => {
+const PORT =
+    process.env.PORT || 3000
 
+server.listen(
+    PORT,
+    "0.0.0.0",
+    () => {
 
-    console.log(
-        "Server running on port 3000"
-    )
-})
+        console.log(
+            `Server running on ${PORT}`
+        )
+    }
+)
+
